345e8ce4c5e4932bfc53ccf58a27915380a415e0
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-05-30 17:25:36 +0900

    fix : server.js middleware 수정

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ const tokenAuthmiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader.split(" ")[1];
 
-  const pack = (req, res, status, message) => {
+  const pack = (status, message) => {
     res.statusCode = status;
     req.tokenDec = message;
   };
 
   if (token === null) {
-    pack(req, res, 401, { tokenMissing: "not exist token" });
+    pack(401, { tokenMissing: "not exist token" });
   } else {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) {
-        pack(req, res, 403, { Error: err });
+        pack(403, { Error: err });
       } else {
-        pack(req, res, 200, { payload });
+        pack(200, { payload });
       }
     });
   }
